ac824c8450bb3eea67d18a1689938e65ab8b2f99
Simon Charest <simoncharest@gmail.com>
2020-10-06 13:58:21 -0400

    Adding comments

--- genealopy/common/file.py
+++ genealopy/common/file.py
@@ -5,15 +5,21 @@ import json
 
 
 def dump_json_objects(filename, json_objects, encoding='utf-8', ensure_ascii=False):
+    """ Write a JSON dictionary object to a file """
+
     with open(filename, 'w', encoding=encoding) as stream:
         json.dump(json_objects, stream, ensure_ascii=ensure_ascii)
 
 
 def dumps(json_objects, ensure_ascii=False):
+    """ Serialize JSON dictionary object and return it as a JSON formatted string """
+
     return json.dumps(json_objects, ensure_ascii=ensure_ascii)
 
 
 def get_filenames(paths, recursive=True):
+    """ Get a list of filenames """
+
     list_ = list()
 
     for path in paths:
@@ -24,12 +30,16 @@ def get_filenames(paths, recursive=True):
 
 
 def loads(string):
+    """ Deserialize a JSON formatted string and return is as a JSON dictionary object  """
+
     json_objects = json.loads(string)
 
     return json_objects
 
 
 def load_json_objects(filenames, encoding='utf-8'):
+    """ Read a file and return its content as a JSON dictionary object """
+
     json_objects = {}
 
     for filename in filenames:
@@ -49,11 +59,15 @@ def load_json_objects(filenames, encoding='utf-8'):
 
 
 def write(filename, string, encoding='utf-8'):
+    """ Write to a file """
+
     with open(filename, 'w', encoding=encoding) as stream:
         stream.write(string)
 
 
 def write_json_objects(filename, json_objects, encoding='utf-8', ensure_ascii=False):
+    """ Serialize JSON dictionary object to a JSON formatted string and write it to a file """
+
     with open(filename, 'w', encoding=encoding) as stream:
         # Serialize JSON dictionary object to a JSON formatted string
         string = json.dumps(json_objects, ensure_ascii=ensure_ascii)
@@ -62,5 +76,7 @@ def write_json_objects(filename, json_objects, encoding='utf-8', ensure_ascii=Fa
 
 
 def read(filename, encoding='utf-8'):
+    """ Read from a file """
+
     with open(filename, 'r', encoding=encoding) as stream:
         return stream.read()
